2e20e841419cf6e5ce7ee4ed1a6075d5f7b9faaf
Samaia Gahramanov <samaiag96@gmail.com>
2024-04-15 15:36:56 +0200

    -second try

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import axios from 'axios';
 import BaseCard from './components/BaseCard.vue';
 import TheCircles from './components/TheCircles.vue';
 import DonutChart from './components/charts/DonutChart.vue';
-import ChartNoVue from './components/charts/ChartNoVue.vue'
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
@@ -17,7 +16,6 @@ app.config.globalProperties.$axios = axios;
 app.component('base-card', BaseCard);
 app.component('the-circles', TheCircles);
 app.component('donut-chart', DonutChart);
-app.component('chart-no-vue', ChartNoVue)
 app.use(createPinia());
 
 app.mount('#app');
